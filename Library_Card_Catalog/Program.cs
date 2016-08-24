76f8e1a2e22a6418c05dbd52cc335cd025cf37d5
Oliver Lykken <olykken@gmail.com>
2016-08-24 16:02:23 -0500

    Adding to serialization

--- Library_Card_Catalog/Program.cs
+++ Library_Card_Catalog/Program.cs
@@ -10,4 +10,28 @@ namespace Library_Card_Catalog
 			
 		}
 	}
+
+	public class Book : ISerializable {
+
+		public string Title{ get; set; }
+		public string Author{ get; set; }
+		public string Genre{ get; set; }
+		public int NumPages { get; set;}
+		public int YearPublished{ get; set; }
+
+		public Book () {
+			//empty constructor
+		}
+
+		public void GetObjectData(SerializationInfo info, StreamingContext context)
+		{
+			// Use the AddValue method to specify serialized values.
+			info.AddValue(this.Title, Title, typeof(string));
+			info.AddValue (this.Author, Author, typeof(string));
+			info.AddValue(this.Genre, Genre, typeof(string));
+			info.AddValue (this.NumPages, NumPages, typeof(int));
+			info.AddValue (this.YearPublished, YearPublished, typeof(int));
+
+		}
+	}
 }
